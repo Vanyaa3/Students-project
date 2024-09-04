0f60098076d453adc85d4ee01814c4637fb68ce6
Vanyaa3 <85151263+Vanyaa3@users.noreply.github.com>
2024-09-04 14:07:30 +0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagmentSystem.Models;
+using System.Diagnostics;
+
+namespace StudentManagmentSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
